172cff01df60091b167e97211eda2ca92ad1fcbd
Jemd13 <jemd311@gmail.com>
2017-12-09 14:55:26 -0800

    Pushing readme and my code

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# DataMining
-Repository for the Data Mining Project
+The code we're submitting for the project includes multiple .py programs. 
+
+Each program represents a specific algorithm and will run a training process and output a set of predictions corresponding
+to the given test data set.
+
+We're also including "combineOutputs.py", a program that can be run by using python3 combineOutputs.py file1 file2 file3 ... fileX , and it'll produce an output file that represents the average of the predicted scores of all the files in the list. We used this in the project in order to obtain the ensemble average of the predictions, which ended up giving us a better score in Kaggle.
---
+++ SVDpp.py
@@ -0,0 +1,52 @@
+import sys
+import operator
+import re, string
+import csv
+import math
+import numpy as np
+from scipy import sparse
+import pandas as pd
+from scipy.sparse import lil_matrix
+from surprise import NMF
+from surprise import SVD
+from surprise import SVDpp
+from surprise import Dataset
+from surprise import evaluate, print_perf
+from surprise import Reader
+
+
+numUsers = 693208
+numItems = 145302
+
+# Main program	    		
+if __name__ == '__main__':
+
+	# Read csv into a pandas dataframe
+	dfRatings = pd.read_csv(sys.argv[1])
+	dfTest = pd.read_csv(sys.argv[2])
+
+	# Delete unused columns
+	del dfRatings['date']
+	del dfRatings['train_id']
+	del dfTest['date']
+	del dfTest['test_id']
+
+
+	# Set the rating scale and create the data for Surprise to use
+	reader = Reader(rating_scale=(1, 5))
+	data = Dataset.load_from_df(dfRatings[['user_id', 'business_id', 'rating']], reader)
+
+	train_set = data.build_full_trainset() 
+
+	# Use SVD with surprise
+	algo = SVDpp()
+	algo.train(train_set)
+
+	f = open('SVDOutput.csv','w')
+	f.write("test_id,rating\n")
+	for i in range(len(dfTest)) :
+		prediction = algo.predict(dfTest.at[i,'user_id'],dfTest.at[i,'business_id'],r_ui=4,verbose=True)
+		predRating = prediction.est
+		f.write(str(i)+","+str(predRating)+'\n')
+
+	f.close()
