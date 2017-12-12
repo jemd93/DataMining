9b07a39a55fdd4749995c9fb79daf6e308c22f3e
brieh <briehoffman@gmail.com>
2017-12-11 16:01:05 -0800

    Co_clustering implementation

---
+++ co_clustering.py
@@ -0,0 +1,68 @@
+# This code to predict ratings for user-item pairs from the Yelp dataset 
+# using the co-clustering algorithm provided by the Surprise library
+# Written by Brie Hoffman, Nov 4 2017 for CMPT 741 final project
+
+
+import pandas as pd
+import sys
+from surprise import CoClustering, Dataset, Reader, evaluate, GridSearch
+
+# import training set as a pandas dataframe
+train_file_path = sys.argv[1]
+#train_file_path = r'/home/brieh/train_rating.txt'
+dftrain = pd.read_csv(train_file_path)
+dftrain = dftrain.drop(['train_id', 'date'], axis=1)
+
+# import test set as a pandas dataframe
+test_file_path = sys.argv[2]  
+#test_file_path = r'/home/brieh/test_rating.txt'
+dftest = pd.read_csv(test_file_path)
+dftest = dftest.drop(['test_id', 'date'], axis=1)
+
+# create a trainset object 
+reader = Reader()
+data = Dataset.load_from_df(dftrain, reader)
+trainset = data.build_full_trainset()
+
+
+#param_grid = {'n_cltr_u': [2,3,4,5,6,7,8,9,10],
+#              'n_cltr_i': [2,3,4,5,6,7,8,9,10],
+#              'n_epochs': [10,20,30,40,50,60,70,80,90,100]}
+param_grid = {'n_cltr_u': [3],
+              'n_cltr_i': [3],
+              'n_epochs': [10,20,35,65,80,90]}
+
+
+# create a co-clustering algorithm
+algo = CoClustering(n_epochs=100)
+algo.train(trainset)
+
+
+# Evaluate the model with 5-fold cross validation
+#data.split(5)
+
+
+#grid_search = GridSearch(CoClustering, param_grid, measures=['RMSE'])
+#grid_search.evaluate(data)
+#print ("after grid_search.evaluate(data)")
+#print_perf(perf)
+
+
+#results_df = pd.DataFrame.from_dict(grid_search.cv_results)
+#print(results_df)
+
+
+
+f = open('testOutput.csv','w')
+f.write("test_id,rating\n")
+for i in range(len(dftest)) :
+    prediction = algo.predict(dftest.at[i,'user_id'],dftest.at[i,'business_id'],r_ui=4,verbose=True)
+    predRating = prediction.est
+    f.write(str(i)+","+str(predRating)+'\n')
+
+f.close()
+
+
+
+
+
