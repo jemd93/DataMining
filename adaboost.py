0efc9e21c12fb3675e8ff93f1209dae9945d9724
brieh <brieh@sfu.ca>
2017-12-11 16:17:44 -0800

    Minor changes

--- adaboost.py
+++ adaboost.py
@@ -1,57 +1,83 @@
 import pandas as pd
 import numpy as np
-import sys
+import sys, random
 from surprise import SVD, Dataset, Reader, evaluate, GridSearch
+
+
+# draw: [float] -> int
+# pick an index from the given list of floats proportionally
+# to the size of the entry (i.e. normalize to a probability
+# distribution and draw according to the probabilities).
+def draw(weights):
+    choice = random.uniform(0, sum(weights))
+    choiceIndex = 0
+    for weight in weights:
+        choice -= weight
+        if choice <= 0:
+            return choiceIndex
+        choiceIndex += 1
+    assert False, "Shouldn't get here"
 
 
 # compute the weighted error of a given hypothesis on a distribution
 def computeError(h, examples, weights=None):
     if weights is None:
-        weights = [1.] * len(examples)
+        weights = [1.] * l
+    for i in range(l):
+        abserr[i] = math.abs(examples.at[i,'rating'] - algo.predict(examples.at[i,'user_id'],examples.at[i,'business_id']).est)
     hypothesisResults = [h(x)*y for (x,y) in examples] # +1 if correct, else -1
 
     return hypothesisResults, sum(w for (z,w) in zip(hypothesisResults, weights) if z < 0)
 
-
-
-# import training set as a pandas dataframe
-#train_file_path = sys.argv[1]
-train_file_path = r'/home/brieh/Documents/DataMining/project/data/DataMining/train_rating.txt'
-examples = pd.read_csv(train_file_path)
-examples = examples.drop(['train_id', 'date'], axis=1)
-
-
-
-# create a trainset object
-reader = Reader()
-data = Dataset.load_from_df(examples, reader)
-trainset = data.build_full_trainset()
-
-dfTest = examples.drop(['rating'], axis=1)
-
-
 # boost: [(list, label)], learner, int -> (list -> label)
 # where a learner is (() -> (list, label)) -> (list -> label)
 # boost the weak learner into a strong learner
-def boost(examples, weakLearner, rounds=10):
-   distr = normalize([1.] * len(examples))
-   hypotheses = [None] * rounds
-   alpha = [0] * rounds
+def boost(examples, rounds=10):
+    distr = normalize([1.] * l)
+    hypotheses = [None] * rounds
+    alpha = [0] * rounds
 
-   for t in range(rounds):
-    # Use SVD with surprise
-      algo = SVD()
-      algo.train(data)
+    for t in range(rounds):
 
-      hypotheses[t] = algo
-      hypothesisResults, error = computeError(hypotheses[t], examples, distr)
+        #create a training set based on the weight distribution
+        for i in range(l):
+            examples[i] = examples[draw(distr)]
 
-      alpha[t] = 0.5 * math.log((1 - error) / (.0001 + error))
-      distr = normalize([d * math.exp(-alpha[t] * h)
-                         for (d,h) in zip(distr, hypothesisResults)])
-      print("Round %d, error %.3f" % (t, error))
+        # create a trainset object
+        reader = Reader()
+        data = Dataset.load_from_df(examples, reader)
+        trainset = data.build_full_trainset()
 
-   def finalHypothesis(x):
-      return sign(sum(a * h(x) for (a, h) in zip(alpha, hypotheses)))
+        # Use SVD with surprise
+        algo = SVD()algo.train(trainset)
+        hypotheses[t] = algo
 
-   return finalHypothesis
+        for i in range(l):
+            abserr[i] = math.abs(examples.at[i,'rating'] - algo.predict(examples.at[i,'user_id'],examples.at[i,'business_id']).est)
+
+        # update weights 
+        delta = sum(x*y for x,y in zip(distr,abserr) if abserr > delta)
+        hypRes = np.where(abserr > delta,-1,1)
+        alpha[t] = 0.5 * math.log((1 - delta) / (.0001 + delta))
+
+        distr = normalize([d * math.exp(-alpha[t] * h) for (d,h) in zip(distr, hypRes)]) 
+
+       
+    def finalHypothesis(x):
+        return sign(sum(a * h(x) for (a, h) in zip(alpha, hypotheses))) 
+
+    return finalHypothesis
+
+
+def main():
+    # import training set as a pandas dataframe
+    train_file_path = r'/home/brieh/Documents/DataMining/project/data/DataMining/train_rating.txt'
+    dfTrain = pd.read_csv(train_file_path)
+    dfTrain = dfTrain.drop(['train_id', 'date'], axis=1)
+    l = len(dfTrain)
+
+    adaboostHyp = boost(dfTrain)
+
+
+if __name__ == '__main__':
+    main(input,output)
